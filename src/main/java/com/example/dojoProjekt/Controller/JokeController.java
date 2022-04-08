264a18d4f12ca627f3133ca71cfebb2c7fd82763
Katarzyna Grocholska <katarzyna.grocholska@ppuc.pl>
2022-04-08 09:24:08 +0200

    fix

--- src/main/java/com/example/dojoProjekt/Controller/JokeController.java
+++ src/main/java/com/example/dojoProjekt/Controller/JokeController.java
@@ -3,10 +3,11 @@ package com.example.dojoProjekt.Controller;
 
 import com.example.dojoProjekt.Service.JokeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = "https://api.chucknorris.io/jokes/random")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/joke")
